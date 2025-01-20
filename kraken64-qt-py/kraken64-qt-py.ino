34b909ab041aed4f5d56eeb9a77961a69e5c5df6
jasoncoon <3598755+jasoncoon@users.noreply.github.com>
2025-01-20 09:05:13 -0600

    WS2816 Test

--- kraken64-qt-py/kraken64-qt-py.ino
+++ kraken64-qt-py/kraken64-qt-py.ino
@@ -24,7 +24,6 @@ FASTLED_USING_NAMESPACE
 #define LED_TYPE WS2816
 #define COLOR_ORDER GRB
 #define NUM_LEDS 64
-#define NUM_LEDS2 NUM_LEDS * 2
 
 #include "GradientPalettes.h"
 #include "Map.h"
@@ -33,7 +32,6 @@ FASTLED_USING_NAMESPACE
 #define FRAMES_PER_SECOND 240
 
 CRGB leds[NUM_LEDS];
-CRGB leds2[NUM_LEDS2];
 
 uint8_t brightness = 128;
 
@@ -57,28 +55,13 @@ void setup()
   // Serial.println("serial delay...");
   // delay(3000);
 
-  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds2, NUM_LEDS2);
+  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
   FastLED.setDither(false);
   // FastLED.setCorrection(TypicalSMD5050);
   FastLED.setBrightness(brightness);
   // FastLED.setMaxPowerInVoltsAndMilliamps(5, MILLI_AMPS);
   fill_solid(leds, NUM_LEDS, CRGB::Black);
   show();
-}
-
-void show() {
-  for (uint16_t i = 0; i < NUM_LEDS; i++)
-  {
-    CRGB led = leds[i];
-    uint8_t r = led.r;
-    uint8_t g = led.g;
-    uint8_t b = led.b;
-
-    leds2[i * 2] = CRGB(r, r, g);
-    leds2[i * 2 + 1] = CRGB(g, b, b);
-  }
-
-  FastLED.show();
 }
 
 void loop()
@@ -102,7 +85,7 @@ void loop()
 
   colorWaves();
 
-  show();
+  FastLED.show();
 }
 
 // ColorWavesWithPalettes by Mark Kriegsman: https://gist.github.com/kriegsman/8281905786e8b2632aeb
